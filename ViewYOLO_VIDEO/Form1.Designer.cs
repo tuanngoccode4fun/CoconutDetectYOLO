b5a24b1234c0f69d1fb9c4b35b77bf3d1ef4e4db
Admin <Admin@ADMIN>
2021-03-27 15:46:39 +0700

    Add function yolo detect video .mp4

---
+++ ViewYOLO_VIDEO/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ViewYOLO_VIDEO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Input = new System.Windows.Forms.GroupBox();
+            this.BT_START = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBoxView = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.BT_STOP = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.Input.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.Input, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxView, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.69535F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.30465F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1067, 631);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // Input
+            // 
+            this.Input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Input.Controls.Add(this.BT_STOP);
+            this.Input.Controls.Add(this.BT_START);
+            this.Input.Controls.Add(this.textBox1);
+            this.Input.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Input.Location = new System.Drawing.Point(3, 3);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(1061, 99);
+            this.Input.TabIndex = 1;
+            this.Input.TabStop = false;
+            this.Input.Text = "Input";
+            // 
+            // BT_START
+            // 
+            this.BT_START.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_START.ForeColor = System.Drawing.Color.Navy;
+            this.BT_START.Location = new System.Drawing.Point(8, 16);
+            this.BT_START.Name = "BT_START";
+            this.BT_START.Size = new System.Drawing.Size(165, 37);
+            this.BT_START.TabIndex = 1;
+            this.BT_START.Text = "START";
+            this.BT_START.UseVisualStyleBackColor = true;
+            this.BT_START.Click += new System.EventHandler(this.BT_START_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(195, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(860, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "D:\\07_YEAR_2021\\00_start_run_video\\1610743648509843890.mp4";
+            // 
+            // pictureBoxView
+            // 
+            this.pictureBoxView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxView.Location = new System.Drawing.Point(3, 108);
+            this.pictureBoxView.Name = "pictureBoxView";
+            this.pictureBoxView.Size = new System.Drawing.Size(1061, 520);
+            this.pictureBoxView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxView.TabIndex = 2;
+            this.pictureBoxView.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // BT_STOP
+            // 
+            this.BT_STOP.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_STOP.ForeColor = System.Drawing.Color.Red;
+            this.BT_STOP.Location = new System.Drawing.Point(7, 56);
+            this.BT_STOP.Name = "BT_STOP";
+            this.BT_STOP.Size = new System.Drawing.Size(165, 37);
+            this.BT_STOP.TabIndex = 2;
+            this.BT_STOP.Text = "STOP";
+            this.BT_STOP.UseVisualStyleBackColor = true;
+            this.BT_STOP.Click += new System.EventHandler(this.BT_STOP_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1073, 638);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "DectectCoconutByYOLO";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.Input.ResumeLayout(false);
+            this.Input.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox Input;
+        private System.Windows.Forms.Button BT_START;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBoxView;
+        private System.Windows.Forms.Button BT_STOP;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ViewYOLO_VIDEO/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Alturos.Yolo;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp.Extensions;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ViewYOLO_VIDEO
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture videocapture;
+        int yoloWidth = 608, yoloHeight = 608;
+        Mat image;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BT_START_Click(object sender, EventArgs e)
+        {
+            if(!backgroundWorker1.IsBusy)
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var configurationDetector = new ConfigurationDetector();
+            var config = configurationDetector.Detect();
+            YoloWrapper yoloWrapper = new YoloWrapper(config);
+            // OpenCV & WPF setting
+
+            image = new Mat();
+
+            //WriteableBitmap wb = new WriteableBitmap(yoloWidth, yoloHeight, 96, 96, PixelFormats.Bgr24, null);
+            var bgWorker = (BackgroundWorker)sender;
+            byte[] imageInBytes = new byte[(int)(yoloWidth * yoloHeight * image.Channels())];
+            videocapture = new VideoCapture(textBox1.Text);
+            while (!bgWorker.CancellationPending)
+            {
+
+                    using (Mat imageOriginal = new Mat())
+                    {
+                        // read a single frame and convert the frame into a byte array
+                        videocapture.Read(imageOriginal);
+                        if (imageOriginal.Empty())
+                            break;
+                        image = imageOriginal.Resize(new OpenCvSharp.Size(yoloWidth, yoloHeight));
+                        imageInBytes = image.ToBytes();
+
+                        // conduct object detection and display the result
+                        var items = yoloWrapper.Detect(imageInBytes);
+                        foreach (var item in items)
+                        {
+                            var x = item.X;
+                            var y = item.Y;
+                            var width = item.Width;
+                            var height = item.Height;
+                            var type = item.Type;  // class name of the object
+
+                            // draw a bounding box for the detected object
+                            // you can set different colors for different classes
+                            Cv2.Rectangle(image, new OpenCvSharp.Rect(x, y, width, height), Scalar.Green, 3);
+                            Cv2.PutText(image, type, new OpenCvSharp.Point(x - 10, y), HersheyFonts.HersheyComplex, 0.5, Scalar.Red);
+                        }
+                        bgWorker.ReportProgress(0,image);
+                        //// display the detection result
+                        //WriteableBitmapConverter.ToWriteableBitmap(image, wb);
+                        ///* WPF component: videoViewer
+                        //<Canvas Name="canvasYoloVideo" Height="608" Width="608">
+                        //  <Image Name="videoViewer" Height="608" Width="608" Stretch="Fill" />
+                        //</Canvas>
+                        //*/
+                        //videoViewer.Source = wb;
+
+                    }
+            }
+            // Read a video file and run object detection over it!
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            var frameBitmap = (Mat)e.UserState;
+            pictureBoxView.Image?.Dispose();
+            pictureBoxView.Image = frameBitmap.ToBitmap();
+        }
+
+        private void BT_STOP_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy)
+                backgroundWorker1.CancelAsync();
+        }
+    }
+}
